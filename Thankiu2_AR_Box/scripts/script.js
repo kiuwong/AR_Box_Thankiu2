9daab65284a17bf974b1de143e3456d79f53b9bc
kiuwong <kiumwong@gmail.com>
2019-06-13 01:21:54 -0400

    completed AR Effect

--- Thankiu2_AR_Box/scripts/script.js
+++ Thankiu2_AR_Box/scripts/script.js
@@ -2,22 +2,67 @@ const Scene = require('Scene');
 const Reactive = require('Reactive');
 const TouchGestures = require('TouchGestures')
 const NativeUI = require('NativeUI');
-const Patches = require('Patches');
+const Time = require('Time');
+const Diagnostics = require('Diagnostics');
 
 
 const kiuModel = Scene.root.find('scene');
 const planeTracker = Scene.root.find('planeTracker0');
-const editButton = Scene.root.find('plane1');
+const editButton = Scene.root.find('editButton');
+const lightButton = Scene.root.find('lightButton')
+const lights = Scene.root.find('ambientLight1')
 const textbox = Scene.root.find('text1');
-
+const launchScreen = Scene.root.find('launchScreen')
+const displayScreen = Scene.root.find('displayScreen')
+const logoScreen = Scene.root.find('logoScreen')
 
 Scene.root.find('ambientLight0').intensity = require('LightingEstimation').frameBrightness;
 
+//Hide Initial Objects
+launchScreen.hidden = false;
+displayScreen.hidden = true;
+kiuModel.hidden = true;
+logoScreen.hidden = true;
+lights.hidden = true;
 
 TouchGestures.onTap().subscribe(function(gesture) {
 	planeTracker.trackPoint(gesture.location);
 });
 
+TouchGestures.onTap().subscribe(hideLaunchScreen);
+
+
+function hideLaunchScreen() { 
+	launchScreen.hidden = true;
+	displayScreen.hidden = false;
+	kiuModel.hidden = false;
+	logoScreen.hidden = false;
+	stopTimer();
+};
+
+const intervalTimer = Time.setInterval(hideLaunchScreen, 7000)
+
+function stopTimer() {
+	Time.clearInterval(intervalTimer);
+}
+
+const timeoutTimer = Time.setTimeout(stopTimer, 8000);
+
+TouchGestures.onTap(lightButton).subscribe(turnOnLights);
+
+let lightState = 1
+
+function turnOnLights() { 
+	if(lightState == 1) {
+		lights.hidden = false;
+		lightState = 0;
+	} else {
+		lights.hidden = true;
+		lightState = 1;
+	}	
+};
+
+//User Functionalities
 
 TouchGestures.onPan(planeTracker).subscribe(function(gesture) {
 	planeTracker.trackPoint(gesture.location, gesture.state);
@@ -39,6 +84,7 @@ TouchGestures.onRotate(kiuModel).subscribe(function(gesture) {
   kiuModel.transform.rotationY = Reactive.add(lastRotationY, Reactive.mul(-1, gesture.rotation));
 });
 
+//Texting Capabilities
 
 TouchGestures.onTap(editButton).subscribe(function() {
 	NativeUI.enterTextEditMode('Name');
